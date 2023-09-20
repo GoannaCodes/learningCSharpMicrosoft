b2b23ba4128870a88edaea668ac3858eea6be35f
GoannaCodes <anguy100@hotmail.com>
2023-09-20 18:50:41 +1000

    Variable scope exercise

--- 7-codeblocks/TestProject/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿bool flag = true;
-int value = 0; //now accessible to both scopes
-if (flag){
-    value = 10;
-    Console.WriteLine($"Inside of code block: {value}");
-}
-Console.WriteLine($"Outside of code block: {value}"); 
---
+++ 8-variablescope/Program.cs
@@ -0,0 +1,21 @@
+﻿/* Challenge: update problematic code */
+
+int[] numbers = {4, 8, 15, 16, 23, 42};
+//declare variables outside so they can be used in local scope as well
+int total = 0;
+bool found = false; //since it hasn't been found yet
+foreach (int number in numbers){
+    
+    total += number;
+
+    if (number == 42){
+        found = true;
+    }
+}
+
+if (found)
+{
+    Console.WriteLine("Set contains 42");
+}
+
+Console.WriteLine($"Total: {total}");
