b95a4e0d4116bd0ae895962a35669be96794d228
GoannaCodes <anguy100@hotmail.com>
2023-09-23 03:49:04 +1000

    Placeholder menu text and fixing indenting

--- 8-guidedProj-branching-loop/GuidedProject/Starter/Program.cs
+++ 8-guidedProj-branching-loop/GuidedProject/Starter/Program.cs
@@ -8,7 +8,7 @@ string animalNickname = "";
 
 // variables that support data entry
 int maxPets = 8;
-string? readResult;
+string readResult;
 string menuSelection = "";
 
 // array used to store runtime data, there is no persisted data
@@ -71,7 +71,7 @@ for (int i = 0; i < maxPets; i++)
 
 do
 {
-// display the top-level menu options
+    // display the top-level menu options
 
     Console.Clear();
 
@@ -99,25 +99,56 @@ do
     // // pause code execution
     // readResult = Console.ReadLine();
 
-    switch(menuSelection)
+    switch (menuSelection)
     {
         case "1":
+            Console.WriteLine("this app feature is coming soon - please check back to see progress.");
+            Console.WriteLine("Press the Enter key to continue.");
             break;
+
         case "2":
+            Console.WriteLine("this app feature is coming soon - please check back to see progress.");
+            Console.WriteLine("Press the Enter key to continue.");
             break;
+
         case "3":
+            Console.WriteLine("Challenge Project - please check back soon to see progress.");
+            Console.WriteLine("Press the Enter key to continue.");
+            readResult = Console.ReadLine();
             break;
+
         case "4":
+            Console.WriteLine("Challenge Project - please check back soon to see progress.");
+            Console.WriteLine("Press the Enter key to continue.");
+            readResult = Console.ReadLine();
             break;
+
         case "5":
+            Console.WriteLine("UNDER CONSTRUCTION - please check back next month to see progress.");
+            Console.WriteLine("Press the Enter key to continue.");
+            readResult = Console.ReadLine();
             break;
+
         case "6":
+            Console.WriteLine("UNDER CONSTRUCTION - please check back next month to see progress.");
+            Console.WriteLine("Press the Enter key to continue.");
+            readResult = Console.ReadLine();
             break;
+
         case "7":
+            Console.WriteLine("UNDER CONSTRUCTION - please check back next month to see progress.");
+            Console.WriteLine("Press the Enter key to continue.");
+            readResult = Console.ReadLine();
             break;
+
         case "8":
+            Console.WriteLine("UNDER CONSTRUCTION - please check back next month to see progress.");
+            Console.WriteLine("Press the Enter key to continue.");
+            readResult = Console.ReadLine();
             break;
+
         default:
-            break;    
+            break;
     }
 } while (menuSelection != "exit");
+
